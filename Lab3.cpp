6b32957d4d10cecb1bfb4a5979379c0bfd94e92e
Julia Shats <julia7476@gmail.com>
2019-10-15 18:26:25 +0300

    Create template for lab3

---
+++ Lab3.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+using namespace std;
+
+struct Collection {
+	int elno;
+	int *elements;
+};
+
+void AddToCollection(Collection &col, int element) {
+	//todo 
+}
+
+void PrintCollection(Collection col) {
+	cout << "[ ";
+	for (int i = 0; i < col.elno; i++)
+		cout << col.elements[i] << " ";
+	cout << "]" << endl;
+}
+
+int main() {
+	Collection collection = { 0, NULL };
+	
+	int elems;
+	cout << "How many elements? ";
+	cin >> elems;
+	srand(time(NULL));
+	for (int i = 0; i < elems; i++)
+		AddToCollection(collection, rand() % 100 + 1);
+	PrintCollection(collection);
+	delete[] collection.elements;
+	return 0;
+}
